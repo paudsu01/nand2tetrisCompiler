054652fb6a9576191a56b0d71285a3076e437fd1
Supreme Paudel <supremepaudel49@gmail.com>
2024-09-21 17:49:52 -0500

    Add token.py

---
+++ token.py
@@ -0,0 +1,60 @@
+from enum import Enum
+import re
+
+class Token:
+
+    SYMBOLS = ['{','}','(',')','[',']','.',',',';','+','-',\
+            '*','/','&','|','<','>','=','~']
+    KEYWORDS = ['class', 'constructor', 'function', 'method', 'field',\
+                'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false',\
+                'null', 'this','let', 'do', 'if', 'else', 'while', 'return']
+
+    def __init__(self, raw_code: str):
+
+        if raw_code in self.KEYWORDS:
+            self.__token_type = TokenType.KEYWORD
+            self.__value = raw_code
+
+        elif raw_code in self.SYMBOLS:
+            self.__token_type = TokenType.SYMBOL
+            self.__value = raw_code
+
+        elif self.__token_is_integer_constant(raw_code):
+            self.__token_type = TokenType.INTEGER_CONSTANT
+            self.__value = raw_code
+
+        elif self.__token_is_string_constant(raw_code):
+            self.__token_type = TokenType.STRING_CONSTANT
+            self.__value = raw_code
+
+        else:
+            self.__token_type = TokenType.IDENTIFIER
+            self.__value = raw_code
+
+    def __token_is_string_constant(self, raw_value: str) -> bool:
+        pattern1 = re.compile(r'^".*?"$')
+        pattern2 = re.compile(r"^'.*?'$")
+        return True if pattern1.match(raw_value) or pattern2.match(raw_value) else False
+
+    def __token_is_integer_constant(self, raw_value: str) -> bool:
+        try:
+            int(raw_value)
+            return True
+        except:
+            return False
+
+    @property
+    def token_type(self):
+        return self.__token_type
+
+    @property
+    def value(self):
+        return self.__value
+
+class TokenType(Enum):
+     
+    KEYWORD = 1
+    SYMBOL = 2
+    INTEGER_CONSTANT = 3
+    STRING_CONSTANT = 4
+    IDENTIFIER = 5
